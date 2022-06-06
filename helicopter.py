1d3629a3e47a154031dd3fd809230fd4b06761d8
fikremariamF <fikremariamfikadud54@gmail.com>
2022-06-06 15:56:51 +0300

    added movement to the helicopter and constructed the basic structure of the helicopter

--- helicopter.py
+++ helicopter.py
@@ -1,17 +1,41 @@
 from cmath import cos, sin
-from ctypes.wintypes import RGB
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
+import math
 
 
 
 class Helicopter:
-    def __init__(self,x,y, wn) -> None:
-        self.wn = wn
+    def __init__(self,x,y) -> None:
         self.x = x
         self.y = y
     def draw(self):
-        pygame.draw.circle(self.wn,(0,0,0), (self.x,self.y), 50)
+        glClear(GL_COLOR_BUFFER_BIT)
+        glColor3f(1.0, 1.0, 1.0)
+        glEnable(GL_BLEND)
+        glPointSize(10)
+        glBegin(GL_POLYGON)
+        rad = 160
+        teta = 0.0
+        while teta < 361:
+            x = rad * cos(teta)
+            y = rad * sin(teta)
+            x = x.real
+            y = y.real
+            glVertex2d(x + self.x,
+                       y + self.y)
+            teta = teta + 1
+        glColor4f(0.0, 0.0, 0.0, 0.0)
+
+        glEnd()
+        glBegin(GL_LINE_LOOP)
+        glVertex2f(-600 + self.x, -60 +self.y)
+        glVertex2f(-600 + self.x, 20+self.y)
+        glVertex2f(-190 + self.x, 25+self.y)
+        glVertex2f(-200+ self.x, -5 +self.y)
+        glVertex2f(-190 + self.x, -70+self.y)
+        glEnd()
+        glFlush()
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pickle import GLOBAL
-from turtle import color
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
@@ -7,8 +5,16 @@ from OpenGL.GLU import *
 import helicopter
 import numpy as np
 
+
+x = [400, 400]
+
 # def init():
-    
+#     pygame.init()
+#     display = (500, 500)
+#     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+#     glClearColor(0.7, 0.9, 1.0, 1.0)
+#     gluOrtho2D(0, 2000.0, 0, 2000.0)
+#     glClear(GL_COLOR_BUFFER_BIT)
 # def background():
 #     glClear(GL_COLOR_BUFFER_BIT)
 #     glColor3f(1.0, 1.0, 1.0)
@@ -16,24 +22,40 @@ import numpy as np
 #     glBegin(GL_POINT)
 #     glEnd()
 
-
+ 
 def main():
+    # init()
     pygame.init()
-    display = (500, 500)
+    display = (800, 800)
     wn = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     glClearColor(0.7, 0.9, 1.0, 1.0)
-    gluOrtho2D(-1000.0, 1000.0, -1000.0, 1000.0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 4000.0, 0, 4000.0)
     glClear(GL_COLOR_BUFFER_BIT)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        p = pygame.key.get_pressed()
 
+        pygame.draw.rect(wn,(255,255,255),(20,20, 100,200),)
+        if p[K_w] or p[K_UP]:
+            # print("up")
+            x[1] = x[1] + 2
+        if p[K_s] or p[K_DOWN]:
+            x[1] = x[1] -2
+        if p[K_a] or p[K_LEFT]:
+            x[0] = x[0] - 2
+        if p[K_d] or p[K_RIGHT]:
+            x[0] = x[0] + 2
         # background()
-        wn = pygame.display.set_mode((500, 500), DOUBLEBUF | OPENGL)
-        draw = helicopter.Helicopter(-110, -500, wn)
+        print(x)
+        draw = helicopter.Helicopter(x[0],x[1])
         draw.draw()
+        # pygame.display.update()
         pygame.display.flip()
         pygame.time.wait(1)
 
